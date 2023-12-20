0dfc0b0562279a40b289ae0500415dbe2d020972
JAVID ASHWAQ F <112797329+Javid8048@users.noreply.github.com>
2023-12-20 07:00:02 +0000

    Express.js practise 5.09

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
+app.use(express.static("./publicFolder"));
 
 app.get('/', (req, res) => {
     res.status(200).send("Success /")
 });
+app.get('/webPage', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, "./webPage/webPageIndex.html"))
+})
+
 app.all("*", (req, res) => {
     res.status(500).send("<h1 style='text-align: center'>404 page not found</h1>")
 })
+
+
 
 app.listen(5000);
